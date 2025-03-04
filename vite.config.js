5718dffebd62d2f57a5c8fa1d8bcfa2921eb70d9
Pedro Lorenzo Rosario <senpai@Pedros-MacBook-Pro.local>
2025-03-04 17:18:39 -0400

    updated

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Movie-Theater-AdminPanel/",
   plugins: [react(),  tailwindcss()],
 })
